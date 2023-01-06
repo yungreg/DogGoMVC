1d882d5198580dcc32ba757e1c8f1315a422052e
Reginald Williams Jr <rlwjr433@gmail.com>
2023-01-06 09:10:44 -0600

    Dog Table NULL values updated

---
+++ DogTableUpdate.sql
@@ -0,0 +1,45 @@
+﻿select * from Dog
+
+--UPDATE this imaugeurl 
+--https://www.svgrepo.com/show/417297/dog.svg
+
+--UPDATE Dog 
+--Set Notes = 'Good Dog!', ImageUrl = 'https://www.svgrepo.com/show/417297/dog.svg'
+--where Id = 1
+
+
+--UPDATE Dog 
+--Set Notes = 'Curious Dog!', ImageUrl = 'https://www.svgrepo.com/show/417297/dog.svg'
+--where Id = 2
+
+--UPDATE Dog 
+--Set Notes = 'Energetic Dog!', ImageUrl = 'https://www.svgrepo.com/show/417296/wolf.svg'
+--where Id = 3
+
+--UPDATE Dog 
+--Set Notes = 'Relaxed Dog!', ImageUrl = 'https://www.svgrepo.com/show/417296/wolf.svg'
+--where Id = 4
+
+--UPDATE Dog 
+--Set Notes = 'Cherrful Dog!', ImageUrl = 'https://www.svgrepo.com/show/417297/dog.svg'
+--where Id = 5
+
+--UPDATE Dog 
+--Set Notes = 'Protective Dog!', ImageUrl = 'https://www.svgrepo.com/show/417297/dog.svg'
+--where Id = 6
+
+--UPDATE Dog 
+--Set Notes = 'Nice Dog!', ImageUrl = 'https://www.svgrepo.com/show/417297/dog.svg'
+--where Id = 7
+
+--UPDATE Dog 
+--Set Notes = 'Strong Dog!', ImageUrl = 'https://www.svgrepo.com/show/417297/dog.svg'
+--where Id = 8
+
+--UPDATE Dog 
+--Set Notes = 'Cute Dog!', ImageUrl = 'https://www.svgrepo.com/show/417297/dog.svg'
+--where Id = 9
+
+--UPDATE Dog 
+--Set Notes = 'Smart Dog!', ImageUrl = 'https://www.svgrepo.com/show/417297/dog.svg'
+--where Id = 10
